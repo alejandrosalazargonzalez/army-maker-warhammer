d3760e384e535259a9381c98d2aa5761c1cfda91
salazar <alejandrosalazargonzalez2004@gmail.com>
2025-04-04 18:04:22 +0100

    issue #8 acabado

--- src/main/java/es/alejandrosalazargonzalez/army_maker_warhammer/controller/registrarController.java
+++ src/main/java/es/alejandrosalazargonzalez/army_maker_warhammer/controller/registrarController.java
@@ -1,17 +1,23 @@
 
 package es.alejandrosalazargonzalez.army_maker_warhammer.controller;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import es.alejandrosalazargonzalez.army_maker_warhammer.PrincipalApplication;
 import es.alejandrosalazargonzalez.army_maker_warhammer.controller.abstractas.AbstractController;
 import es.alejandrosalazargonzalez.army_maker_warhammer.model.UsuarioEntity;
 import es.alejandrosalazargonzalez.army_maker_warhammer.model.UsuarioServiceModel;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 /**
  *   @author: alejandrosalazargonzalez
@@ -26,15 +32,17 @@ public class registrarController extends AbstractController {
     @FXML private TextField correoTextField;
     @FXML private TextField correoTextField2;
     @FXML private Label errorText;
+    @FXML private Button buttonAcetarRegistrar;
+    @FXML private Button buttonAtras;
 
     @FXML
     private void onButtonAceptarRegClick(){
-        if (usuarioTextField == null || usuarioTextField.toString().isEmpty()
-        || nombreTextField == null || nombreTextField.toString().isEmpty()
-        || contraseniaTextField == null || contraseniaTextField.toString().isEmpty()
-        || contraseniaTextField2 == null || contraseniaTextField2.toString().isEmpty()
-        || correoTextField == null || correoTextField.toString().isEmpty()
-        || correoTextField2 == null || correoTextField2.toString().isEmpty()) {
+        if (usuarioTextField == null || usuarioTextField.getText().isEmpty()
+        || nombreTextField == null || nombreTextField.getText().isEmpty()
+        || contraseniaTextField == null || contraseniaTextField.getText().isEmpty()
+        || contraseniaTextField2 == null || contraseniaTextField2.getText().isEmpty()
+        || correoTextField == null || correoTextField.getText().isEmpty()
+        || correoTextField2 == null || correoTextField2.getText().isEmpty()) {
             errorText.setText("ninguno de los valores puede estar vacio");
             return;
         }
@@ -52,6 +60,22 @@ public class registrarController extends AbstractController {
             return;
         }
         getUsuarioServiceModel().addUsuario(nuevoUsuario);
+    }
+
+    @FXML
+    private void registrarToLoginOnClick(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(PrincipalApplication.class.getResource("app-init.fxml"));
+            Stage stage = (Stage) buttonAtras.getScene().getWindow();
+            Scene scene;
+            scene = new Scene(fxmlLoader.load(), 510, 900);
+            stage.setResizable(false);
+            stage.setTitle("Pantalla Princial");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
