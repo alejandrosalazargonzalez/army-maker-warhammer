1553211a7c857fe399e7f30f52c9d60a9baaf0fd
alejandrosalazargonzalez <alejandrosalazargonzalez2004@gmail.com>
2025-05-02 16:23:51 +0100

    avances en las ultimas pantallas

--- src/main/java/es/alejandrosalazargonzalez/army_maker_warhammer/controller/OpcionesController.java
+++ src/main/java/es/alejandrosalazargonzalez/army_maker_warhammer/controller/OpcionesController.java
@@ -1,10 +1,39 @@
 
 package es.alejandrosalazargonzalez.army_maker_warhammer.controller;
 
+import es.alejandrosalazargonzalez.army_maker_warhammer.controller.abstractas.AbstractController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 /**
  * @author: alejandrosalazargonzalez
  * @version: 1.0.0
  */
-public class OpcionesController {
+public class OpcionesController extends AbstractController{
+
+    @FXML private Button crearEjercitoButton;
+    @FXML private Button crearPublicacionButton;
+    @FXML private Button opcionesCuentaButton;
+    @FXML private Button inicio;
+
+    @FXML
+    private void opcionesToCrearEjercitoOnClick(){
+        cambiarPantalla(crearEjercitoButton, "crearEjercito", "opciones");
+    }
+
+    @FXML
+    private void opcionesToCrearPubOnClick(){
+        cambiarPantalla(crearPublicacionButton, "crearPublicacion", "opciones");
+    }
+
+    @FXML
+    private void opcionesToOpcionesCuentaOnClick(){
+        cambiarPantalla(opcionesCuentaButton, "opcionesCuenta", "opciones");
+    }
+
+    @FXML
+    private void opcionToInicioOnClick(){
+        cambiarPantalla(inicio, "app-init", "opciones");
+    }
 
 }
---
+++ src/main/java/es/alejandrosalazargonzalez/army_maker_warhammer/model/GeneralEntity.java
@@ -0,0 +1,5 @@
+package es.alejandrosalazargonzalez.army_maker_warhammer.model;
+
+public class GeneralEntity {
+    
+}
